00f92c3da182c430cc120474bf2a7ff9f2a73089
Samsul-Alam-STP <samsul.alam.stp@gmail.com>
2023-06-10 00:23:48 +0600

    initial full projects

---
+++ src/components/Hero.jsx
@@ -0,0 +1,175 @@
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
+
+const Section = styled.div`
+  min-height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  @media only screen and (max-width: 768px) {
+    min-height: 200vh;
+  }
+`;
+
+const Container = styled.div`
+  height: 100%;
+  scroll-snap-align: center;
+  max-width: 1400px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 50px;
+  padding-bottom: 50px;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    align-items: center;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    text-align: center;
+  }
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    width: 100%;
+  }
+`;
+
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+    height: 300px;
+  }
+
+  @keyframes animate {
+    to {
+      transform: translateY(20px);
+    }
+  }
+`;
+
+const Hero = () => {
+  return (
+    <Section>
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Learn. Grow. Achieve.</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What Am I ?</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            I passionately craft stunning websites that eloquently convey the
+            intended message.
+          </Desc>
+          {/* <Button>About Me</Button> */}
+          <Button
+            onClick={() =>
+              window.open(
+                "https://samsul-alam-stp.github.io/profile/",
+                "_blank"
+              )
+            }
+          >
+            About Me
+          </Button>
+        </Left>
+        <Right>
+          <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight intensity={1} />
+              <directionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={2.4}>
+                <MeshDistortMaterial
+                  color="#3d1c56"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas>
+          <Img src="./img/moon.png" />
+        </Right>
+      </Container>
+    </Section>
+  );
+};
+
+export default Hero;
